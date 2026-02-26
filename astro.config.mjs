b98725ffb8d95d39cc0e632d39117e10016b0641
Mike Riley <mikeriley131@gmail.com>
2026-02-25 17:16:11 -0700

    client feedback feb 23

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,4 @@
 import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.google(),
-        name: "Archivo",
-        cssVariable: "--font-archivo",
-        weights: ["700"],
-      },
-      {
-        provider: fontProviders.google(),
-        name: "Raleway",
-        cssVariable: "--font-raleway",
-        weights: ["400 500"],
-      },
-    ],
-  },
-});
+export default defineConfig({});
