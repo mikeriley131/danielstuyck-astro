e564485568d79682bb6462db48818fa7e7f6aa93
Mike Riley <mikeriley131@gmail.com>
2026-03-25 23:25:24 -0600

    preview test 2

--- src/lib/api.js
+++ src/lib/api.js
@@ -243,3 +243,19 @@ export async function getPreviewPostByDatabaseId(databaseId) {
 
   return data?.post;
 }
+
+export async function testAuth() {
+  const data = await fetchAPI(
+    `
+    {
+      viewer {
+        databaseId
+        name
+      }
+    }
+    `,
+    { auth: true },
+  );
+
+  return data;
+}
