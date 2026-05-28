5010415d31e9c1100c49283021d4252e1693c44c
Mike Riley <mikeriley131@gmail.com>
2026-05-27 19:55:19 -0600

    intial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,32 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
-  image: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "admin.danielstuyck.com",
-      },
-    ],
-  },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "PT Serif",
+      cssVariable: "--font-pt-serif",
+      styles: ["normal"],
+      weights: ["400", "700"],
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Amaranth",
+      cssVariable: "--font-amaranth",
+      styles: ["normal"],
+      weights: ["400"],
+    },
+  ],
+  // image: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "admin.wordpresssite.com",
+  //     },
+  //   ],
+  // },
 });
---
+++ stylelint.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('stylelint').Config} */
+
+export default {
+  extends: ["stylelint-config-standard-scss"],
+  rules: {
+    "value-keyword-case": null,
+  },
+};
